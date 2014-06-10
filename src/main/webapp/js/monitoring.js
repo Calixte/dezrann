f75612b408e36dc8dd5b1d49a6cab477cb315ba9
Clément Guillaume <clementguillaume56@gmail.com>
2014-06-09 18:06:47 -0700

    log watcher

---
+++ src/main/webapp/js/monitoring.js
@@ -0,0 +1,28 @@
+var consoleDiv = document.getElementById('console');
+function checkConfig(config) {
+	if (config == undefined) {
+		config = {}
+	}
+	if (config.host == undefined) {
+		config.host = 'localhost';
+	}
+	if (config.port == undefined) {
+		config.port = '8080';
+	}
+	if (config.path == undefined) {
+		config.path = '/echo';
+	}
+	if (config.secure == undefined) {
+		config.secure = false;
+	}
+	return config;
+}
+function watcherInit(id, c) {
+	var config = checkConfig(c);
+	var url = (config.secure ? 'wss' : 'ws') + '://' + config.host + ':' + config.port + config.path;
+	var ws = new WebSocket(url);
+	ws.send(id);
+	ws.onmessage = function (messageEvent) {
+		consoleDiv.textContent += '<br>' + messageEvent.data;
+	}
+}
--- src/main/webapp/watch.jsp
+++ src/main/webapp/watch.jsp
@@ -4,8 +4,13 @@
 <head>
 	<title>Dezrann - User behaviour analysis</title>
 	<meta charset="utf-8" />
+	<script src="js/monitoring.js"></script>
+	<script>watcherInit(${session.id}, {path: 'watch'})</script>
 </head>
 <body>
-<h1>You are monitoring ${session.getId()}</h1>
+<h1>You are monitoring ${session.id}</h1>
+<div id="console">
+
+</div>
 </body>
 </html>
